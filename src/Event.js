1fc75a2fa0172a227ea6f5f102b3ad3f110a10a4
seohyun1257 <hwangseohyun1234@naver.com>
2025-08-16 19:50:47 +0900

    first commit

---
+++ src/Event.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Event extends Component {
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+      </div>
+    );
+  }
+}
+
+export default Event;
